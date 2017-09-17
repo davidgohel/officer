ae13c4b0ae09b1c96c400adda2d0dbdb99d55cc0
Jon Calder <jonmcalder@gmail.com>
2017-09-17 12:22:44 +0200

    Document package object

---
+++ R/officer.R
@@ -0,0 +1,17 @@
+#' officer: Manipulate Microsoft Word and PowerPoint Documents
+#'
+#' The officer package facilitates access to and manipulation of
+#' 'Microsoft Word' and 'Microsoft PowerPoint' documents from R.
+#'
+#' \itemize{
+#' \item read Word and PowerPoint files into data objects
+#' \item add/edit/remove image, table and text content from documents and slides
+#' \item write updated content back to Word and PowerPoint files
+#' }
+#'
+#' To learn more about officer, start with the vignettes:
+#' `browseVignettes(package = "officer")`
+#'
+#' @docType package
+#' @name officer
+"_PACKAGE"
