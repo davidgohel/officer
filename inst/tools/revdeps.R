d54ed4371ee8e652d784c17b12f1fa163c5f69c6
dgohel <david.gohel@ardata.fr>
2017-12-01 15:17:38 +0100

    revdep_scr

---
+++ inst/tools/revdeps.R
@@ -0,0 +1,20 @@
+rev_dep_lib <- "xxx"
+
+install.packages("huxtable", lib = rev_dep_lib, dependencies = c("Depends", "Imports", "LinkingTo", "Suggests") )
+
+officers <- unique( c("magrittr",
+"lazyeval","purrr","tibble","R6","stringr",
+"htmltools","htmlwidgets",
+"gdtools", "rmarkdown", "knitr",
+"xml2", "dplyr",
+"testthat","data.table",
+"cellranger",
+"writexl" ) )
+install.packages(officers, lib = rev_dep_lib,
+                 dependencies = c("Depends", "Imports", "LinkingTo", "Suggests") )
+install.packages(c("cowplot", "stringi", "weights") , lib = rev_dep_lib,
+                 dependencies = c("Depends", "Imports", "LinkingTo", "Suggests") )
+
+
+library(devtools)
+revdep_check(libpath = rev_dep_lib, ignore = "flextable")
