512ee94505851273f7d4de8fee85aaf7c4dc8e13
dgohel <david.gohel@ardata.fr>
2018-03-05 13:58:03 +0100

    pkgdown

--- inst/tools/pkgdown.R
+++ inst/tools/pkgdown.R
@@ -3,4 +3,12 @@ pkgdown::build_site()
 browseURL("docs/articles/assets/docx/toc_and_captions.docx")
 browseURL("docs/articles/assets/docx/body_add_demo.docx")
 browseURL("docs/articles/assets/docx/slip_in_demo.docx")
-browseURL("docs/articles/assets/docx/section.docx")
+
+all_files <- list.files(path = "docs/reference/", pattern = "\\.html", full.names = TRUE)
+library(purrr)
+library(stringr)
+walk(all_files, .f = function(file){
+  content <- readLines(file, encoding = "UTF-8")
+  content <- str_replace_all(content, "/Users/davidgohel/Github/officer/docs/reference/", "/.../")
+  cat(content, file = file, sep = "\n")
+})
