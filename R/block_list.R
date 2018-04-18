91ebfaecf45e45ecdf7018e445621961d3ef28e4
dgohel <david.gohel@ardata.fr>
2018-04-18 13:58:49 +0200

    block_list doc

--- R/block_list.R
+++ R/block_list.R
@@ -1,4 +1,21 @@
 #' @export
+#' @title list of blocks
+#' @description a list of blocks can be used to gather
+#' several blocks (paragraphs or tables) into a single
+#' object. The function is to be used when adding
+#' footnotes for example.
+#' @param ... a list of objects of class \code{fpar} or
+#' \code{flextable}.
+#' @examples
+#' img.file <- file.path( R.home("doc"), "html", "logo.jpg" )
+#' bl <- block_list(
+#'   fpar(ftext("hello world", shortcuts$fp_bold())),
+#'   fpar(
+#'     ftext("hello", shortcuts$fp_bold()),
+#'     stext(" world", "strong"),
+#'     external_img(src = img.file, height = 1.06, width = 1.39)
+#'   )
+#' )
 block_list <- function(...){
   x <- list(...)
   class(x) <- "block_list"
@@ -10,11 +27,6 @@ print.block_list <- function(x, ...){
     cat(format(i, type = "console"), "\n")
   }
   invisible()
-}
-
-#' @export
-add_block <- function(x, value ){
-  x
 }
 
 
