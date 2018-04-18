591a785ce9c1b99acd397955238f1cf16c1a5825
dgohel <david.gohel@ardata.fr>
2018-04-18 19:44:02 +0200

    tests

--- tests/testthat/test-docx-replace.R
+++ tests/testthat/test-docx-replace.R
@@ -1,7 +1,7 @@
 context("replace elements in an rdocx")
 
 
-test_that("docx replace_at bkm", {
+test_that("replace bkm in body", {
   doc <- read_docx() %>%
     body_add_par("centered text", style = "centered") %>%
     slip_in_text(". How are you", style = "strong") %>%
@@ -14,12 +14,33 @@ test_that("docx replace_at bkm", {
 
   doc <- body_replace_at(doc, "text_to_replace", "not left aligned")
   xmldoc <- doc$doc_obj$get()
-  xpath_ <- sprintf("//w:bookmarkStart[@w:name='%s']", "text_to_replace")
   newtext <- xml_find_first(xmldoc, "w:body/w:p[2]/w:r")
   expect_equal(xml_text(newtext), "not left aligned")
 
   lasttext <- xml_find_first(xmldoc, "w:body/w:p[2]/w:r[2]")
   expect_equal(xml_text(lasttext), ". How are you")
+})
+
+test_that("replace bkm in headers and footers", {
+
+  doc <- read_docx(path = "docs_dir/table-complex.docx" )
+  doc <- headers_replace_at(doc, "hello1", "salut")
+  doc <- footers_replace_at(doc, "hello2", "salut")
+
+  xmldoc <- doc$headers[[1]]$get()
+  xpath_ <- sprintf("//w:bookmarkStart[@w:name='%s']", "hello1")
+  bm_start <- xml_find_first(xmldoc, xpath_)
+  expect_false( inherits(bm_start, "xml_missing"))
+  newtext <- xml_find_first(xmldoc, "/w:hdr/w:p[1]/w:r")
+  expect_equal(xml_text(newtext), "salut")
+
+  xmldoc <- doc$footers[[1]]$get()
+  xpath_ <- sprintf("//w:bookmarkStart[@w:name='%s']", "hello2")
+  bm_start <- xml_find_first(xmldoc, xpath_)
+  expect_false( inherits(bm_start, "xml_missing"))
+  newtext <- xml_find_first(xmldoc, "/w:ftr/w:p[1]/w:r[3]")
+  expect_equal(xml_text(newtext), "salut")
+
 })
 
 test_that("docx replace text", {
@@ -30,5 +51,20 @@ test_that("docx replace text", {
                                only_at_cursor = FALSE, ignore.case = TRUE)
   xmldoc <- doc$doc_obj$get()
   expect_equal(xml_text( xml_find_all(xmldoc, "//w:p") ), c("", "new one", "new two") )
+})
+
+test_that("docx replace all text", {
+  doc <- read_docx(path = "docs_dir/table-complex.docx" )
+
+  doc <- headers_replace_all_text(doc, old_value = "hello", new_value = "salut",
+                               only_at_cursor = FALSE, ignore.case = TRUE)
+  doc <- footers_replace_all_text(doc, old_value = "hello", new_value = "salut",
+                               only_at_cursor = FALSE, ignore.case = TRUE)
+
+  xmldoc <- doc$headers[[1]]$get()
+  expect_equal(xml_text( xml_find_all(xmldoc, "//w:p") ), "salut world" )
+
+  xmldoc <- doc$footers[[1]]$get()
+  expect_equal(xml_text( xml_find_all(xmldoc, "//w:p") ), "world salut" )
 })
 
