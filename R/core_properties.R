193151d5dc5b0c73f20cec75f4c0c68caf56e880
David Gohel <david.gohel@ardata.fr>
2019-04-29 19:17:16 +0200

    improve core_properties

--- R/core_properties.R
+++ R/core_properties.R
@@ -23,16 +23,45 @@ read_core_properties <- function( package_dir ){
   props <- matrix(c(sapply(names_, function(x) x[1] ), propnames, attrs, xml_text(all_)), ncol = 4 )
   colnames(props) <- c("ns", "name", "attrs", "value")
   rownames(props) <- propnames
-  attr(props, "ns") <- unclass(ns_)
-  props
+  z <- list(data = props, ns = unclass(ns_))
+  class(z) <- "core_properties"
+  z
+}
+
+default_cp_tags <- list(
+  title = list(ns = "dc", attrs = NA_character_),
+  subject = list(ns = "dc", attrs = NA_character_),
+  creator = list(ns = "dc", attrs = NA_character_),
+  keywords = list(ns = "cp", attrs = NA_character_),
+  description = list(ns = "dc", attrs = NA_character_),
+  lastModifiedBy = list(ns = "cp", attrs = NA_character_),
+  revision = list(ns = "cp", attrs = NA_character_),
+  created = list(ns = "dcterms", attrs = 'xsi:type="dcterms:W3CDTF"'),
+  modified = list(ns = "dcterms", attrs = 'xsi:type="dcterms:W3CDTF"'),
+  category = list(ns = "cp", attrs = NA_character_)
+)
+
+`[<-.core_properties` <- function( x, i, j, value ){
+  if( !i %in% x$data[,"name"] ) {
+    attrs <- ifelse(is.na(default_cp_tags[[i]]$attrs), "", paste0(" ", default_cp_tags[[i]]$attrs))
+    new <- matrix( c(default_cp_tags[[i]]$ns, i, attrs, value), nrow = 1, dimnames = list(i, colnames(x$data)))
+    x$data <- rbind(x$data, new)
+  } else {
+    x$data[i,j] <- value
+  }
+  x
+}
+`[.core_properties` <- function( x, i, j ){
+  x$data[i,j]
 }
 
 write_core_properties <- function(core_matrix, package_dir){
+  ns_ <- core_matrix$ns
+  core_matrix <- core_matrix$data
   if(!is.matrix(core_matrix)){
     stop("core_properties should be stored in a character matrix.")
   }
 
-  ns_ <- attr(core_matrix, "ns")
   ns_ <- paste0('xmlns:', names(ns_), '=\"', ns_, '\"', collapse = " ")
   xml_ <- paste0('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>', '\n',
                  '<cp:coreProperties ', ns_, '>')
