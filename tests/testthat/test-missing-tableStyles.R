2644a00365e379f64bc2dd04b25da75149990996
David Gohel <david.gohel@ardata.fr>
2019-11-04 14:30:43 +0100

    minor

--- tests/testthat/test-missing-tableStyles.R
+++ tests/testthat/test-missing-tableStyles.R
@@ -5,6 +5,7 @@ download.file("https://ndownloader.figshare.com/files/16252631",
               destfile = destfile)
 
 test_that("missing tableStyles file in pptx from figshare", {
+  skip_if_not(file.exists(destfile))
   expect_warning({
     x <- read_pptx(destfile)
   })
