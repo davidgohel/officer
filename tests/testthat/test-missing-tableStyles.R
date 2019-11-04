d100ca17fa85be827389a9769c1c2ce0e06419f3
David Gohel <david.gohel@ardata.fr>
2019-11-04 14:58:30 +0100

    skip as file can not be unz on windows

--- tests/testthat/test-missing-tableStyles.R
+++ tests/testthat/test-missing-tableStyles.R
@@ -5,7 +5,7 @@ download.file("https://ndownloader.figshare.com/files/16252631",
               destfile = destfile)
 
 test_that("missing tableStyles file in pptx from figshare", {
-  skip_if_not(file.exists(destfile))
+  testthat::skip_on_os("windows")
   expect_warning({
     x <- read_pptx(destfile)
   })
