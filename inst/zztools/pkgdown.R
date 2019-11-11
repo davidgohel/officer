8aba5f45debdf3bc115baca4a18cb618d591c109
David Gohel <david.gohel@ardata.fr>
2019-11-11 10:39:56 +0100

    pkgdown command

--- inst/zztools/pkgdown.R
+++ inst/zztools/pkgdown.R
@@ -1,10 +1,10 @@
 # unlink("vignettes/offcran/assets", recursive = TRUE, force = TRUE)
 pkgdown::build_site()
-
-browseURL("vignettes/offcran/assets/docx/toc_and_captions.docx")
-browseURL("vignettes/offcran/assets/docx/body_add_demo.docx")
-browseURL("vignettes/offcran/assets/docx/slip_in_demo.docx")
-
 file.copy("vignettes/offcran/assets", to = "docs/articles/offcran", overwrite = TRUE, recursive = TRUE)
+
+browseURL("docs/articles/offcran/assets/docx/toc_and_captions.docx")
+browseURL("docs/articles/offcran/assets/docx/body_add_demo.docx")
+browseURL("docs/articles/offcran/assets/docx/slip_in_demo.docx")
+
 unlink("vignettes/offcran/assets", recursive = TRUE, force = TRUE)
 unlink("vignettes/offcran/extract.png", recursive = TRUE, force = TRUE)
