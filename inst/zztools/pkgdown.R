56f5e2df643dce8860b4c47cc80b1fa9b415f3f3
David Gohel <david.gohel@ardata.fr>
2020-05-04 01:18:30 +0200

    doc

--- inst/zztools/pkgdown.R
+++ inst/zztools/pkgdown.R
@@ -4,7 +4,6 @@ file.copy("vignettes/offcran/assets", to = "docs/articles/offcran", overwrite = 
 
 browseURL("docs/articles/offcran/assets/docx/toc_and_captions.docx")
 browseURL("docs/articles/offcran/assets/docx/body_add_demo.docx")
-browseURL("docs/articles/offcran/assets/docx/slip_in_demo.docx")
 
 unlink("vignettes/offcran/assets", recursive = TRUE, force = TRUE)
 unlink("vignettes/offcran/extract.png", recursive = TRUE, force = TRUE)
