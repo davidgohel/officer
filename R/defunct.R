9f7db57bd5558f8599241b05ee419a6ac809b15a
David Gohel <david.gohel@ardata.fr>
2021-03-04 16:10:01 +0100

    keep ph_with_gg_at ph_with_table_at ph_with_text for RtutoR

--- R/defunct.R
+++ R/defunct.R
@@ -20,41 +20,8 @@ ph_with_table_at <- function(...){
 
 #' @export
 #' @rdname officer-defunct
-#' @details `ph_with_img_at()` is replaced by `ph_with.external_img`.
-#' @param ... unused arguments
-ph_with_img_at <- function(...){
-  .Defunct("ph_with")
-}
-
-#' @export
-#' @rdname officer-defunct
-#' @details `ph_with_img()` is replaced by `ph_with.external_img`.
-#' @param ... unused arguments
-ph_with_img <- function(...){
-  .Defunct("ph_with")
-}
-
-#' @export
-#' @rdname officer-defunct
 #' @details `ph_with_text()` is replaced by `ph_with.character`.
 #' @param ... unused arguments
 ph_with_text <- function(...){
   .Defunct("ph_with")
 }
-
-#' @export
-#' @rdname officer-defunct
-#' @details `ph_empty_at()` is replaced by `ph_with.empty_content`.
-#' @param ... unused arguments
-ph_empty_at <- function(...){
-  .Defunct("ph_with")
-}
-
-#' @export
-#' @rdname officer-defunct
-#' @details `ph_empty()` is replaced by `ph_with.empty_content`.
-#' @param ... unused arguments
-ph_empty <- function(...){
-  .Defunct("ph_with")
-}
-
