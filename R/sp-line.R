85f8fff82661daf2dc2e0a4386133fbdf13df159
stefan <moogs@gmx.de>
2022-03-13 11:24:18 +0100

    Move check_set_class to formatting_properties.R

--- R/sp-line.R
+++ R/sp-line.R
@@ -1,14 +1,3 @@
-# check properties helpers ----
-
-check_set_class <- function( obj, value, cl){
-  varname <- as.character(substitute(value))
-  cl_str <- sprintf(" must be a %s object.", cl)
-  if( !inherits( value, cl ) )
-    stop(varname, cl_str, call. = FALSE)
-  else obj[[varname]] <- value
-  obj
-}
-
 # line ----
 
 #' @title Line properties
