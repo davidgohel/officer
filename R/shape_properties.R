b88f7fd7b5959163590e67405395ce4a84a16ca8
stefan <moogs@gmx.de>
2022-03-13 17:33:15 +0100

    Rename function.

--- R/shape_properties.R
+++ R/shape_properties.R
@@ -1,3 +1,73 @@
+# check properties helpers ----
+
+check_set_shape <- function(x) {
+  # http://www.datypic.com/sc/ooxml/t-a_ST_ShapeType.html
+  shape_types <- c(
+    "line", "lineInv", "triangle",
+    "rtTriangle", "rect", "diamond", "parallelogram",
+    "trapezoid", "nonIsoscelesTrapezoid", "pentagon", "hexagon",
+    "heptagon", "octagon", "decagon", "dodecagon", "star4",
+    "star5", "star6", "star7", "star8", "star10", "star12",
+    "star16", "star24", "star32", "roundRect", "round1Rect",
+    "round2SameRect", "round2DiagRect", "snipRoundRect",
+    "snip1Rect", "snip2SameRect", "snip2DiagRect", "plaque", "ellipse",
+    "teardrop", "homePlate", "chevron", "pieWedge", "pie",
+    "blockArc", "donut", "noSmoking", "rightArrow",
+    "leftArrow", "upArrow", "downArrow", "stripedRightArrow",
+    "notchedRightArrow", "bentUpArrow", "leftRightArrow",
+    "upDownArrow", "leftUpArrow", "leftRightUpArrow", "quadArrow",
+    "leftArrowCallout", "rightArrowCallout", "upArrowCallout",
+    "downArrowCallout", "leftRightArrowCallout",
+    "upDownArrowCallout", "quadArrowCallout", "bentArrow", "uturnArrow",
+    "circularArrow", "leftCircularArrow",
+    "leftRightCircularArrow", "curvedRightArrow", "curvedLeftArrow",
+    "curvedUpArrow", "curvedDownArrow", "swooshArrow", "cube", "can",
+    "lightningBolt", "heart", "sun", "moon", "smileyFace",
+    "irregularSeal1", "irregularSeal2", "foldedCorner", "bevel",
+    "frame", "halfFrame", "corner", "diagStripe", "chord", "arc",
+    "leftBracket", "rightBracket", "leftBrace",
+    "rightBrace", "bracketPair", "bracePair", "straightConnector1",
+    "bentConnector2", "bentConnector3", "bentConnector4",
+    "bentConnector5", "curvedConnector2", "curvedConnector3",
+    "curvedConnector4", "curvedConnector5", "callout1", "callout2",
+    "callout3", "accentCallout1", "accentCallout2",
+    "accentCallout3", "borderCallout1", "borderCallout2",
+    "borderCallout3", "accentBorderCallout1", "accentBorderCallout2",
+    "accentBorderCallout3", "wedgeRectCallout",
+    "wedgeRoundRectCallout", "wedgeEllipseCallout", "cloudCallout", "cloud",
+    "ribbon", "ribbon2", "ellipseRibbon", "ellipseRibbon2",
+    "leftRightRibbon", "verticalScroll", "horizontalScroll",
+    "wave", "doubleWave", "plus", "flowChartProcess",
+    "flowChartDecision", "flowChartInputOutput",
+    "flowChartPredefinedProcess", "flowChartInternalStorage",
+    "flowChartDocument", "flowChartMultidocument", "flowChartTerminator",
+    "flowChartPreparation", "flowChartManualInput",
+    "flowChartManualOperation", "flowChartConnector",
+    "flowChartPunchedCard", "flowChartPunchedTape", "flowChartSummingJunction",
+    "flowChartOr", "flowChartCollate", "flowChartSort",
+    "flowChartExtract", "flowChartMerge", "flowChartOfflineStorage",
+    "flowChartOnlineStorage", "flowChartMagneticTape",
+    "flowChartMagneticDisk", "flowChartMagneticDrum",
+    "flowChartDisplay", "flowChartDelay", "flowChartAlternateProcess",
+    "flowChartOffpageConnector", "actionButtonBlank",
+    "actionButtonHome", "actionButtonHelp", "actionButtonInformation",
+    "actionButtonForwardNext", "actionButtonBackPrevious",
+    "actionButtonEnd", "actionButtonBeginning",
+    "actionButtonReturn", "actionButtonDocument", "actionButtonSound",
+    "actionButtonMovie", "gear6", "gear9", "funnel", "mathPlus",
+    "mathMinus", "mathMultiply", "mathDivide", "mathEqual",
+    "mathNotEqual", "cornerTabs", "squareTabs", "plaqueTabs",
+    "chartX", "chartStar", "chartPlus"
+  )
+
+  if(!x %in% shape_types) {
+    stop("'", x, "' must be a valid shape type.\n\n",
+         "A valid shape type has to be one of ", paste0(paste0("'", shape_types, "'"), collapse = ", "), ".")
+  } else {
+    return(x)
+  }
+}
+
 # line ----
 
 #' @title Line properties
