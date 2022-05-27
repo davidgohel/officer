davidgohel/officer

commit c45e769eab34a43169766ed45a0333574a060d26

feature: add functionality to set shape geometry and outline
See #409

R/ppt_ph_with_methods.R
@@ -140,11 +140,14 @@ ph_with.character <- function(x, value, location, ...){
   new_ph <- sh_props_pml(left = location$left, top = location$top,
                width = location$width, height = location$height,
                label = location$ph_label, ph = location$ph,
-               rot = location$rotation, bg = location$bg)
+               rot = location$rotation, bg = location$bg,
+               ln = location$ln, geom = location$geom)
+
   pars <- paste0("<a:p><a:r><a:rPr/><a:t>", htmlEscapeCopy(value), "</a:t></a:r></a:p>", collapse = "")
-  xml_elt <- paste0( psp_ns_yes, new_ph,
+  xml_elt <- sprintf(paste0( psp_ns_yes, new_ph,
                      "<p:txBody><a:bodyPr/><a:lstStyle/>",
-                     pars, "</p:txBody></p:sp>" )
+                     pars, "</p:txBody></p:sp>" ))
+
   node <- as_xml_document(xml_elt)
 
   xml_add_child(xml_find_first(slide$get(), "//p:spTree"), node)
@@ -166,7 +169,8 @@ ph_with.numeric <- function(x, value, location, format_fun = format, ...){
   new_ph <- sh_props_pml(left = location$left, top = location$top,
                width = location$width, height = location$height,
                label = location$ph_label, ph = location$ph,
-               rot = location$rotation, bg = location$bg)
+               rot = location$rotation, bg = location$bg,
+               ln = location$ln, geom = location$geom)
 
   pars <- paste0("<a:p><a:r><a:rPr/><a:t>", htmlEscapeCopy(value), "</a:t></a:r></a:p>", collapse = "")
   xml_elt <- paste0( psp_ns_yes, new_ph,
@@ -192,7 +196,8 @@ ph_with.factor <- function(x, value, location, ...){
   new_ph <- sh_props_pml(left = location$left, top = location$top,
                width = location$width, height = location$height,
                label = location$ph_label, ph = location$ph,
-               rot = location$rotation, bg = location$bg)
+               rot = location$rotation, bg = location$bg,
+               ln = location$ln, geom = location$geom)
 
   pars <- paste0("<a:p><a:r><a:rPr/><a:t>", htmlEscapeCopy(value), "</a:t></a:r></a:p>", collapse = "")
   xml_elt <- paste0( psp_ns_yes, new_ph,
@@ -257,11 +262,13 @@ ph_with.block_list <- function(x, value, location, level_list = integer(0), ...)
   new_ph <- sh_props_pml(left = location$left, top = location$top,
                width = location$width, height = location$height,
                label = location$ph_label, ph = location$ph,
-               rot = location$rotation, bg = location$bg)
+               rot = location$rotation, bg = location$bg,
+               ln = location$ln, geom = location$geom)
 
-  xml_elt <- paste0( psp_ns_yes, new_ph,
-                     "<p:txBody><a:bodyPr/><a:lstStyle/>",
-                     pars, "</p:txBody></p:sp>" )
+  xml_elt <- sprintf(paste0( psp_ns_yes, new_ph,
+                             "<p:txBody><a:bodyPr/><a:lstStyle/>",
+                             pars, "</p:txBody></p:sp>" ))
+
   node <- as_xml_document(xml_elt)
 
   xml_add_child(xml_find_first(slide$get(), "//p:spTree"), node)
@@ -283,7 +290,8 @@ ph_with.unordered_list <- function(x, value, location, ...){
   new_ph <- sh_props_pml(left = location$left, top = location$top,
                width = location$width, height = location$height,
                label = location$ph_label, ph = location$ph,
-               rot = location$rotation, bg = location$bg)
+               rot = location$rotation, bg = location$bg,
+               ln = location$ln, geom = location$geom)
 
   xml_elt <- paste0( psp_ns_yes, new_ph,
           "<p:txBody><a:bodyPr/><a:lstStyle/>", p, "</p:txBody></p:sp>" )
@@ -324,7 +332,8 @@ ph_with.data.frame <- function(x, value, location, header = TRUE,
     bt, left = location$left, top = location$top,
     width = location$width, height = location$height,
     label = location$ph_label, ph = location$ph,
-    rot = location$rotation, bg = location$bg)
+    rot = location$rotation, bg = location$bg,
+    ln = location$ln, geom = location$geom)
 
   value <- as_xml_document(xml_elt)
   xml_add_child(xml_find_first(slide$get(), "//p:spTree"), value)
@@ -472,7 +481,8 @@ ph_with.empty_content <- function( x, value, location, ... ){
   new_ph <- sh_props_pml(left = location$left, top = location$top,
                        width = location$width, height = location$height,
                        label = location$ph_label, ph = location$ph,
-                       rot = location$rotation, bg = location$bg)
+                       rot = location$rotation, bg = location$bg,
+                       ln = location$ln, geom = location$geom)
   xml_elt <- paste0( psp_ns_yes, new_ph, "</p:sp>" )
   node <- as_xml_document(xml_elt)
 

NAMESPACE
@@ -55,6 +55,8 @@ S3method(print,ftext)
 S3method(print,rdocx)
 S3method(print,rpptx)
 S3method(print,rxlsx)
+S3method(print,sp_line)
+S3method(print,sp_lineend)
 S3method(print,unordered_list)
 S3method(set_notes,block_list)
 S3method(set_notes,character)
@@ -106,6 +108,8 @@ S3method(update,fp_cell)
 S3method(update,fp_par)
 S3method(update,fp_text)
 S3method(update,fpar)
+S3method(update,sp_line)
+S3method(update,sp_lineend)
 export(add_sheet)
 export(add_slide)
 export(annotate_base)
@@ -233,6 +237,8 @@ export(slip_in_img)
 export(slip_in_seqfield)
 export(slip_in_text)
 export(slip_in_xml)
+export(sp_line)
+export(sp_lineend)
 export(styles_info)
 export(table_colwidths)
 export(table_conditional_formatting)

R/formatting_properties.R
@@ -97,6 +97,15 @@ check_set_choice <- function( obj, value, choices){
   obj
 }
 
+check_set_class <- function( obj, value, cl){
+  varname <- as.character(substitute(value))
+  cl_str <- sprintf(" must be a %s object.", cl)
+  if( !inherits( value, cl ) )
+    stop(varname, cl_str, call. = FALSE)
+  else obj[[varname]] <- value
+  obj
+}
+
 # fp_text ----
 #' @title Text formatting properties
 #'

R/ooxml.R
@@ -37,20 +37,24 @@ runs_to_p_wml <- function(..., add_ns = FALSE, style_id = NULL){
 }
 
 sh_props_pml <- function( left = 0, top = 0, width = 3, height = 3,
-                          bg = "transparent", rot = 0, label = "", ph = "<p:ph/>"){
+                          bg = "transparent", rot = 0, label = "", ph = "<p:ph/>",
+                          ln = sp_line(lwd = 0, linecmpd = "solid", lineend = "rnd"), geom = NULL) {
 
   if( !is.null(bg) && !is.color( bg ) )
     stop("bg must be a valid color.", call. = FALSE )
 
   bg_str <- solid_fill_pml(bg)
+  ln_str <- ln_pml(ln)
+  geom_str <- prst_geom_pml(geom)
 
   xfrm_str <- a_xfrm_str(left = left, top = top, width = width, height = height, rot = rot)
   if( is.null(ph) || is.na(ph)){
     ph = "<p:ph/>"
   }
 
-  str <- "<p:nvSpPr><p:cNvPr id=\"0\" name=\"%s\"/><p:cNvSpPr><a:spLocks noGrp=\"1\"/></p:cNvSpPr><p:nvPr>%s</p:nvPr></p:nvSpPr><p:spPr>%s%s</p:spPr>"
-  sprintf(str, label, ph, xfrm_str, bg_str )
+  str <- "<p:nvSpPr><p:cNvPr id=\"0\" name=\"%s\"/><p:cNvSpPr><a:spLocks noGrp=\"1\"/></p:cNvSpPr><p:nvPr>%s</p:nvPr></p:nvSpPr><p:spPr>%s%s%s%s</p:spPr>"
+
+  sprintf(str, label, ph, xfrm_str, geom_str, bg_str, ln_str)
 
 }
 
@@ -119,6 +123,74 @@ solid_fill_pml <- function(bg){
     bg_str <- solid_fill(bg)
   }
   bg_str
+}
+
+# geom ----
+
+prst_geom_pml <- function(x) {
+  geom_str <- ""
+  if (!is.null(x)) {
+    x <- check_set_geom(x)
+    tagname <- paste0("a:prstGeom")
+    geom_str <- sprintf("<%s prst=\"%s\"><a:avLst/></%s>", tagname, x, tagname)
+  }
+  geom_str
+}
+
+# line ----
+
+ln_pml <- function(x) {
+  ln_str <- ""
+  if (!is.null(x)) {
+    color_ <- ""
+    if(is_transparent(x$color) || x$lwd < .001) {
+      color_ <- "<a:noFill/>"
+    } else {
+      color_ <- solid_fill(x$color)
+    }
+
+    dash_ <- dash_pml(x$lty)
+    join_ <- linejoin_pml(x$linejoin)
+    head_ <- lineend_pml(x$headend, "head")
+    tail_ <- lineend_pml(x$tailend, "tail")
+
+    ln_str <- sprintf(
+      paste0("<a:ln w=\"%s\" cap=\"%s\" cmpd=\"%s\">",
+             color_,
+             dash_,
+             join_,
+             head_,
+             tail_,
+             "</a:ln>"),
+      12700 * x$lwd, x$lineend, x$linecmpd)
+
+  }
+  ln_str
+}
+
+lineend_pml <- function(x, side) {
+  lineend_str <- ""
+  if (!is.null(x)) {
+    tagname <- paste0("a:", side, "End")
+    lineend_str <- sprintf("<%s type=\"%s\" w=\"%s\" len=\"%s\"/>", tagname, x$type, x$width, x$length)
+  }
+  lineend_str
+}
+
+dash_pml <- function(x) {
+  dash_str <- ""
+  if (!is.null(x)) {
+    dash_str <- sprintf("<a:prstDash val=\"%s\"/>", x)
+  }
+  dash_str
+}
+
+linejoin_pml <- function(x) {
+  linejoin_str <- ""
+  if (!is.null(x)) {
+    linejoin_str <- sprintf("<a:%s/>", x)
+  }
+  linejoin_str
 }
 
 # border ----

R/ph_location.R
@@ -71,12 +71,14 @@ fortify_location <- function( x, doc, ... ){
 #' @export
 #' @title Location for a placeholder from scratch
 #' @description The function will return a list that complies with
-#' expected format for argument \code{location} of function\code{ph_with}.
+#' expected format for argument \code{location} of function \code{ph_with}.
 #' @param left,top,width,height place holder coordinates
 #' in inches.
 #' @param newlabel a label for the placeholder. See section details.
 #' @param bg background color
 #' @param rotation rotation angle
+#' @param ln a \code{sp_line} object specifying the outline style.
+#' @param geom shape geometry
 #' @param ... unused arguments
 #' @family functions for placeholder location
 #' @details
@@ -102,14 +104,23 @@ fortify_location <- function( x, doc, ... ){
 #' doc <- ph_with(doc, "Hello world",
 #'   location = ph_location(width = 4, height = 3, newlabel = "hello") )
 #' print(doc, target = tempfile(fileext = ".pptx") )
+#'
+#' # Set geometry and outline
+#' doc <- read_pptx()
+#' doc <- add_slide(doc)
+#' loc <- ph_location(left = 1, top = 1, width = 4, height = 3, bg = "steelblue",
+#'                    ln = sp_line(color = "red", lwd = 2.5),
+#'                    geom = "trapezoid")
+#' doc <- ph_with(doc, "", loc = loc)
+#' print(doc, target = tempfile(fileext = ".pptx") )
 ph_location <- function(left = 1, top = 1, width = 4, height = 3,
                         newlabel = "",
                         bg = NULL, rotation = NULL,
+                        ln = NULL, geom = NULL,
                         ...){
 
   x <- list(left = left, top = top, width = width, height = height,
-    ph_label = newlabel, ph = NA_character_, bg = bg, rotation = rotation)
-
+    ph_label = newlabel, ph = NA_character_, bg = bg, rotation = rotation, ln = ln, geom = geom)
 
   class(x) <- c("location_manual", "location_str")
   x

R/shape_properties.R
@@ -1,0 +1,260 @@
+# check properties helpers ----
+
+check_set_geom <- function(x) {
+  # http://www.datypic.com/sc/ooxml/t-a_ST_ShapeType.html
+  geom_types <- c(
+    "line", "lineInv", "triangle",
+    "rtTriangle", "rect", "diamond", "parallelogram",
+    "trapezoid", "nonIsoscelesTrapezoid", "pentagon", "hexagon",
+    "heptagon", "octagon", "decagon", "dodecagon", "star4",
+    "star5", "star6", "star7", "star8", "star10", "star12",
+    "star16", "star24", "star32", "roundRect", "round1Rect",
+    "round2SameRect", "round2DiagRect", "snipRoundRect",
+    "snip1Rect", "snip2SameRect", "snip2DiagRect", "plaque", "ellipse",
+    "teardrop", "homePlate", "chevron", "pieWedge", "pie",
+    "blockArc", "donut", "noSmoking", "rightArrow",
+    "leftArrow", "upArrow", "downArrow", "stripedRightArrow",
+    "notchedRightArrow", "bentUpArrow", "leftRightArrow",
+    "upDownArrow", "leftUpArrow", "leftRightUpArrow", "quadArrow",
+    "leftArrowCallout", "rightArrowCallout", "upArrowCallout",
+    "downArrowCallout", "leftRightArrowCallout",
+    "upDownArrowCallout", "quadArrowCallout", "bentArrow", "uturnArrow",
+    "circularArrow", "leftCircularArrow",
+    "leftRightCircularArrow", "curvedRightArrow", "curvedLeftArrow",
+    "curvedUpArrow", "curvedDownArrow", "swooshArrow", "cube", "can",
+    "lightningBolt", "heart", "sun", "moon", "smileyFace",
+    "irregularSeal1", "irregularSeal2", "foldedCorner", "bevel",
+    "frame", "halfFrame", "corner", "diagStripe", "chord", "arc",
+    "leftBracket", "rightBracket", "leftBrace",
+    "rightBrace", "bracketPair", "bracePair", "straightConnector1",
+    "bentConnector2", "bentConnector3", "bentConnector4",
+    "bentConnector5", "curvedConnector2", "curvedConnector3",
+    "curvedConnector4", "curvedConnector5", "callout1", "callout2",
+    "callout3", "accentCallout1", "accentCallout2",
+    "accentCallout3", "borderCallout1", "borderCallout2",
+    "borderCallout3", "accentBorderCallout1", "accentBorderCallout2",
+    "accentBorderCallout3", "wedgeRectCallout",
+    "wedgeRoundRectCallout", "wedgeEllipseCallout", "cloudCallout", "cloud",
+    "ribbon", "ribbon2", "ellipseRibbon", "ellipseRibbon2",
+    "leftRightRibbon", "verticalScroll", "horizontalScroll",
+    "wave", "doubleWave", "plus", "flowChartProcess",
+    "flowChartDecision", "flowChartInputOutput",
+    "flowChartPredefinedProcess", "flowChartInternalStorage",
+    "flowChartDocument", "flowChartMultidocument", "flowChartTerminator",
+    "flowChartPreparation", "flowChartManualInput",
+    "flowChartManualOperation", "flowChartConnector",
+    "flowChartPunchedCard", "flowChartPunchedTape", "flowChartSummingJunction",
+    "flowChartOr", "flowChartCollate", "flowChartSort",
+    "flowChartExtract", "flowChartMerge", "flowChartOfflineStorage",
+    "flowChartOnlineStorage", "flowChartMagneticTape",
+    "flowChartMagneticDisk", "flowChartMagneticDrum",
+    "flowChartDisplay", "flowChartDelay", "flowChartAlternateProcess",
+    "flowChartOffpageConnector", "actionButtonBlank",
+    "actionButtonHome", "actionButtonHelp", "actionButtonInformation",
+    "actionButtonForwardNext", "actionButtonBackPrevious",
+    "actionButtonEnd", "actionButtonBeginning",
+    "actionButtonReturn", "actionButtonDocument", "actionButtonSound",
+    "actionButtonMovie", "gear6", "gear9", "funnel", "mathPlus",
+    "mathMinus", "mathMultiply", "mathDivide", "mathEqual",
+    "mathNotEqual", "cornerTabs", "squareTabs", "plaqueTabs",
+    "chartX", "chartStar", "chartPlus"
+  )
+
+  if(!x %in% geom_types) {
+    stop("'", x, "' must be a valid geometry\n\n",
+         "A valid geometry has to be one of ", paste0(paste0("'", geom_types, "'"), collapse = ", "), ".")
+  } else {
+    return(x)
+  }
+}
+
+# line ----
+
+#' @title Line properties
+#'
+#' @description Create a \code{sp_line} object that describes
+#' line properties.
+#'
+#' @param color line color - a single character value specifying
+#' a valid color (e.g. "#000000" or "black").
+#' @param lwd line width (in point) - 0 or positive integer value.
+#' @param lty single character value specifying the line type.
+#' Expected value is one of the following : default \code{'solid'}
+#' or \code{'dot'} or \code{'dash'} or \code{'lgDash'}
+#' or \code{'dashDot'} or \code{'lgDashDot'} or \code{'lgDashDotDot'}
+#' or \code{'sysDash'} or \code{'sysDot'} or \code{'sysDashDot'}
+#' or \code{'sysDashDotDot'}.
+#' @param linecmpd single character value specifying the compound line type.
+#' Expected value is one of the following : default \code{'sng'}
+#' or \code{'dbl'} or \code{'tri'} or \code{'thinThick'}
+#' or \code{'thickThin'}
+#' @param lineend single character value specifying the line end style
+#' Expected value is one of the following : default \code{'rnd'}
+#' or \code{'sq'} or \code{'flat'}
+#' @param linejoin single character value specifying the line join style
+#' Expected value is one of the following : default \code{'round'}
+#' or \code{'bevel'} or \code{'miter'}
+#' @param headend a \code{sp_lineend} object specifying line head end style
+#' @param tailend a \code{sp_lineend} object specifying line tail end style
+#' @return a \code{sp_line} object
+#' @examples
+#' sp_line()
+#' sp_line(color = "red", lwd = 2)
+#' sp_line(lty = "dot", linecmpd = "dbl")
+#' @family functions for defining shape properties
+#' @seealso [sp_lineend]
+#' @export
+sp_line <- function(color = "transparent", lwd = 1, lty = "solid",
+                    linecmpd = "sng", lineend = "rnd", linejoin = "round",
+                    headend = sp_lineend(type = "none"), tailend = sp_lineend(type = "none")) {
+  out <- list()
+  out <- check_set_color(out, color)
+  out <- check_set_numeric(out, lwd)
+  out <- check_set_choice( obj = out, value = lty,
+                           choices = c("solid", "dot", "dash",
+                                       "lgDash", "dashDot", "lgDashDot", "lgDashDotDot", "sysDash",
+                                       "sysDot", "sysDashDot", "sysDashDotDot") )
+  out <- check_set_choice( obj = out, value = linecmpd,
+                           choices = c("sng", "dbl", "tri", "thinThick", "thickThin") )
+  out <- check_set_choice( obj = out, value = lineend,
+                           choices = c("rnd", "sq", "flat") )
+  out <- check_set_choice( obj = out, value = linejoin,
+                           choices = c("round", "bevel", "miter") )
+  out <- check_set_class( obj = out, value = headend, cl = "sp_lineend" )
+  out <- check_set_class( obj = out, value = tailend, cl = "sp_lineend" )
+
+  class( out ) <- c("sp_line")
+
+  out
+}
+
+#' @param x,object \code{sp_line} object
+#' @param ... further arguments - not used
+#' @examples
+#' print( sp_line (color="red", lwd = 2) )
+#' @rdname sp_line
+#' @export
+print.sp_line = function (x, ...){
+  out <- data.frame(
+    color = x$color,
+    lwd = x$lwd,
+    lty = x$lty,
+    linecmpd = x$linecmpd,
+    lineend = x$lineend,
+    linejoin = x$linejoin,
+    headend = unclass(x$headend),
+    tailend = unclass(x$tailend), stringsAsFactors = FALSE )
+  print(out)
+  invisible()
+}
+
+#' @rdname sp_line
+#' @examples
+#' obj <- sp_line (color="red", lwd = 2)
+#' update( obj, linecmpd = "dbl" )
+#' @export
+update.sp_line <- function(object, color, lwd, lty,
+                           linecmpd, lineend, linejoin,
+                           headend, tailend, ...) {
+  if( !missing(color) )
+    object <- check_set_color(object, color)
+  if( !missing(lwd) )
+    object <- check_set_numeric(object, lwd)
+  if( !missing(lty) )
+    object <- check_set_choice( obj = object, value = lty,
+                           choices = c("solid", "dot", "dash",
+                                       "lgDash", "dashDot", "lgDashDot", "lgDashDotDot", "sysDash",
+                                       "sysDot", "sysDashDot", "sysDashDotDot") )
+  if( !missing(linecmpd) )
+  object <- check_set_choice( obj = object, value = linecmpd,
+                           choices = c("sng", "dbl", "tri", "thinThick", "thickThin") )
+  if( !missing(lineend) )
+    object <- check_set_choice( obj = object, value = lineend,
+                           choices = c("rnd", "sq", "flat") )
+
+  if( !missing(linejoin) )
+    object <- check_set_choice( obj = object, value = linejoin,
+                           choices = c("round", "bevel", "miter") )
+
+  if( !missing(headend) )
+    object <- check_set_class( obj = object, value = headend, cl = "sp_lineend" )
+  if( !missing(tailend) )
+    object <- check_set_class( obj = object, value = tailend, cl = "sp_lineend" )
+
+  object
+}
+
+#' @title Line end properties
+#'
+#' @description Create a \code{sp_lineend} object that describes
+#' line end properties.
+#'
+#' @param type single character value specifying the line end type.
+#' Expected value is one of the following : default \code{'none'}
+#' or \code{'triangle'} or \code{'stealth'} or \code{'diamond'}
+#' or \code{'oval'} or \code{'arrow'}
+#' @param width single character value specifying the line end width
+#' Expected value is one of the following : default \code{'sm'}
+#' or \code{'med'} or \code{'lg'}
+#' @param length single character value specifying the line end length
+#' Expected value is one of the following : default \code{'sm'}
+#' or \code{'med'} or \code{'lg'}
+#' @return a \code{sp_lineend} object
+#' @examples
+#' sp_lineend()
+#' sp_lineend(type = "triangle")
+#' sp_lineend(type = "arrow", width = "lg", length = "lg")
+#' @family functions for defining shape properties
+#' @seealso [sp_line]
+#' @export
+sp_lineend <- function(type = "none", width = "med", length = "med") {
+  out <- list()
+  out <- check_set_choice( obj = out, value = type,
+                           choices = c("none","triangle","stealth",
+                                       "diamond","oval","arrow") )
+  out <- check_set_choice( obj = out, value = width,
+                           choices = c("sm", "med", "lg") )
+  out <- check_set_choice( obj = out, value = length,
+                           choices = c("sm", "med", "lg") )
+
+  class( out ) <- c("sp_lineend")
+
+  out
+}
+
+#' @param x,object \code{sp_lineend} object
+#' @param ... further arguments - not used
+#' @examples
+#' print( sp_lineend (type="triangle", width = "lg") )
+#' @rdname sp_lineend
+#' @export
+print.sp_lineend = function (x, ...){
+  out <- data.frame(
+    type = x$type,
+    width = x$width,
+    length = x$length
+  )
+  print(out)
+  invisible()
+}
+
+#' @rdname sp_lineend
+#' @examples
+#' obj <- sp_lineend (type="triangle", width = "lg")
+#' update( obj, type = "arrow" )
+#' @export
+update.sp_lineend <- function(object, type, width, length,
+                           ...) {
+  if( !missing(type) )
+    object <- check_set_choice( obj = object, value = type,
+                                choices = c("none","triangle","stealth",
+                                            "diamond","oval","arrow") )
+  if( !missing(width) )
+    object <- check_set_choice( obj = object, value = width,
+                                choices = c("sm", "med", "lg") )
+  if( !missing(length) )
+    object <- check_set_choice( obj = object, value = length,
+                                choices = c("sm", "med", "lg") )
+
+  object
+}

man/ph_location.Rd
@@ -12,6 +12,8 @@ ph_location(
   newlabel = "",
   bg = NULL,
   rotation = NULL,
+  ln = NULL,
+  geom = NULL,
   ...
 )
 }
@@ -25,11 +27,15 @@ in inches.}
 
 \item{rotation}{rotation angle}
 
+\item{ln}{a \code{sp_line} object specifying the outline style.}
+
+\item{geom}{shape geometry}
+
 \item{...}{unused arguments}
 }
 \description{
 The function will return a list that complies with
-expected format for argument \code{location} of function\code{ph_with}.
+expected format for argument \code{location} of function \code{ph_with}.
 }
 \details{
 The location of the bounding box associated to a placeholder
@@ -54,6 +60,15 @@ doc <- read_pptx()
 doc <- add_slide(doc)
 doc <- ph_with(doc, "Hello world",
   location = ph_location(width = 4, height = 3, newlabel = "hello") )
+print(doc, target = tempfile(fileext = ".pptx") )
+
+# Set geometry and outline
+doc <- read_pptx()
+doc <- add_slide(doc)
+loc <- ph_location(left = 1, top = 1, width = 4, height = 3, bg = "steelblue",
+                   ln = sp_line(color = "red", lwd = 2.5),
+                   geom = "trapezoid")
+doc <- ph_with(doc, "", loc = loc)
 print(doc, target = tempfile(fileext = ".pptx") )
 }
 \seealso{

man/sp_line.Rd
@@ -1,0 +1,90 @@
+% Generated by roxygen2: do not edit by hand
+% Please edit documentation in R/shape_properties.R
+\name{sp_line}
+\alias{sp_line}
+\alias{print.sp_line}
+\alias{update.sp_line}
+\title{Line properties}
+\usage{
+sp_line(
+  color = "transparent",
+  lwd = 1,
+  lty = "solid",
+  linecmpd = "sng",
+  lineend = "rnd",
+  linejoin = "round",
+  headend = sp_lineend(type = "none"),
+  tailend = sp_lineend(type = "none")
+)
+
+\method{print}{sp_line}(x, ...)
+
+\method{update}{sp_line}(
+  object,
+  color,
+  lwd,
+  lty,
+  linecmpd,
+  lineend,
+  linejoin,
+  headend,
+  tailend,
+  ...
+)
+}
+\arguments{
+\item{color}{line color - a single character value specifying
+a valid color (e.g. "#000000" or "black").}
+
+\item{lwd}{line width (in point) - 0 or positive integer value.}
+
+\item{lty}{single character value specifying the line type.
+Expected value is one of the following : default \code{'solid'}
+or \code{'dot'} or \code{'dash'} or \code{'lgDash'}
+or \code{'dashDot'} or \code{'lgDashDot'} or \code{'lgDashDotDot'}
+or \code{'sysDash'} or \code{'sysDot'} or \code{'sysDashDot'}
+or \code{'sysDashDotDot'}.}
+
+\item{linecmpd}{single character value specifying the compound line type.
+Expected value is one of the following : default \code{'sng'}
+or \code{'dbl'} or \code{'tri'} or \code{'thinThick'}
+or \code{'thickThin'}}
+
+\item{lineend}{single character value specifying the line end style
+Expected value is one of the following : default \code{'rnd'}
+or \code{'sq'} or \code{'flat'}}
+
+\item{linejoin}{single character value specifying the line join style
+Expected value is one of the following : default \code{'round'}
+or \code{'bevel'} or \code{'miter'}}
+
+\item{headend}{a \code{sp_lineend} object specifying line head end style}
+
+\item{tailend}{a \code{sp_lineend} object specifying line tail end style}
+
+\item{x, object}{\code{sp_line} object}
+
+\item{...}{further arguments - not used}
+}
+\value{
+a \code{sp_line} object
+}
+\description{
+Create a \code{sp_line} object that describes
+line properties.
+}
+\examples{
+sp_line()
+sp_line(color = "red", lwd = 2)
+sp_line(lty = "dot", linecmpd = "dbl")
+print( sp_line (color="red", lwd = 2) )
+obj <- sp_line (color="red", lwd = 2)
+update( obj, linecmpd = "dbl" )
+}
+\seealso{
+\link{sp_lineend}
+
+Other functions for defining shape properties: 
+\code{\link{sp_lineend}()}
+}
+\concept{functions for defining shape properties}

commit de865ec0172dc0cd72d8cf0a8ea11b444e639265

Fix bug in ph_with methods introduced by PR #409.

R/ppt_ph_with_methods.R
@@ -144,9 +144,9 @@ ph_with.character <- function(x, value, location, ...){
                ln = location$ln, geom = location$geom)
 
   pars <- paste0("<a:p><a:r><a:rPr/><a:t>", htmlEscapeCopy(value), "</a:t></a:r></a:p>", collapse = "")
-  xml_elt <- sprintf(paste0( psp_ns_yes, new_ph,
+  xml_elt <- paste0( psp_ns_yes, new_ph,
                      "<p:txBody><a:bodyPr/><a:lstStyle/>",
-                     pars, "</p:txBody></p:sp>" ))
+                     pars, "</p:txBody></p:sp>" )
 
   node <- as_xml_document(xml_elt)
 
@@ -265,9 +265,9 @@ ph_with.block_list <- function(x, value, location, level_list = integer(0), ...)
                rot = location$rotation, bg = location$bg,
                ln = location$ln, geom = location$geom)
 
-  xml_elt <- sprintf(paste0( psp_ns_yes, new_ph,
-                             "<p:txBody><a:bodyPr/><a:lstStyle/>",
-                             pars, "</p:txBody></p:sp>" ))
+  xml_elt <- paste0( psp_ns_yes, new_ph,
+                     "<p:txBody><a:bodyPr/><a:lstStyle/>",
+                     pars, "</p:txBody></p:sp>" )
 
   node <- as_xml_document(xml_elt)
 