2a06085e32faf7d620707a74cc7fa6ec99505b0b
David Gohel <david.gohel@ardata.fr>
2022-12-19 21:42:52 +0100

    tests: test body_add

--- tests/testthat/test-docx-add.R
+++ tests/testthat/test-docx-add.R
@@ -196,29 +196,29 @@ test_that("add docx into docx", {
 unlink("*.docx")
 unlink("*.emf")
 
+img.file <- file.path( R.home("doc"), "html", "logo.jpg" )
+fpt_blue_bold <- fp_text(color = "#006699", bold = TRUE)
+fpt_red_italic <- fp_text(color = "#C32900", italic = TRUE)
+bl <- block_list(
+  fpar(ftext("hello world", fpt_blue_bold)),
+  fpar(ftext("hello", fpt_blue_bold), " ",
+       ftext("world", fpt_red_italic)),
+  fpar(
+    ftext("hello world", fpt_red_italic),
+    external_img(
+      src = img.file, height = 1.06, width = 1.39)))
+
+anyplot <- plot_instr(code = {
+  col <- c("#440154FF", "#443A83FF", "#31688EFF",
+                      "#21908CFF", "#35B779FF", "#8FD744FF", "#FDE725FF")
+                      barplot(1:7, col = col, yaxt="n")
+})
+
 test_that("visual testing", {
   local_edition(3)
   testthat::skip_if_not_installed("doconv")
   testthat::skip_if_not(doconv::msoffice_available())
   library(doconv)
-  img.file <- file.path( R.home("doc"), "html", "logo.jpg" )
-  fpt_blue_bold <- fp_text(color = "#006699", bold = TRUE)
-  fpt_red_italic <- fp_text(color = "#C32900", italic = TRUE)
-  bl <- block_list(
-    fpar(ftext("hello world", fpt_blue_bold)),
-    fpar(ftext("hello", fpt_blue_bold), " ",
-         ftext("world", fpt_red_italic)),
-    fpar(
-      ftext("hello world", fpt_red_italic),
-      external_img(
-        src = img.file, height = 1.06, width = 1.39)))
-
-  anyplot <- plot_instr(code = {
-    col <- c("#440154FF", "#443A83FF", "#31688EFF",
-             "#21908CFF", "#35B779FF", "#8FD744FF", "#FDE725FF")
-    barplot(1:7, col = col, yaxt="n")
-  })
-
 
   x <- read_docx()
   # add text and a table ----
@@ -232,4 +232,24 @@ test_that("visual testing", {
   x <- body_add_blocks(x = x, blocks = bl)
 
   expect_snapshot_doc(x = x, name = "docx-elements", engine = "testthat")
+})
+
+test_that("body_add visual testing", {
+  local_edition(3)
+  testthat::skip_if_not_installed("doconv")
+  testthat::skip_if_not(doconv::msoffice_available())
+  library(doconv)
+
+  x <- read_docx()
+  # add text and a table ----
+  x <- body_add(x, "Hello World")
+  x <- body_add(x, "Hello title", style = "heading 1")
+  x <- body_add(x, "Hello title", style = "heading 2")
+  x <- body_add(x, head(cars))
+  x <- body_add(x, "Hello base plot", style = "heading 2")
+  x <- body_add(x, anyplot)
+  x <- body_add(x, "Hello fpars", style = "heading 2")
+  x <- body_add(x = x, bl)
+
+  expect_snapshot_doc(x = x, name = "body_add-elements", engine = "testthat")
 })
