868e66a3d0764d6429031444ad891eddbbefbd0c
Nan Xiao <nanxstats@gmail.com>
2023-06-10 09:59:44 -0400

    Skip test requiring internet connection when offline (#504)

--- tests/testthat/test-missing-tableStyles.R
+++ tests/testthat/test-missing-tableStyles.R
@@ -1,14 +1,16 @@
-destfile = tempfile(fileext = ".pptx")
-download.file("https://ndownloader.figshare.com/files/16252631",
-              destfile = destfile)
-
 test_that("missing tableStyles file in pptx from figshare", {
+  testthat::skip_if_offline()
   testthat::skip_on_os("windows")
+
+  destfile <- tempfile(fileext = ".pptx")
+  on.exit(file.remove(destfile), add = TRUE)
+
+  download.file(
+    "https://ndownloader.figshare.com/files/16252631",
+    destfile = destfile
+  )
+
   expect_warning({
     x <- read_pptx(destfile)
   })
-
 })
-
-file.remove(destfile)
-
