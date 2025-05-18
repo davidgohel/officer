7949cf58a37a2ea78d4931ca07abc1256415db2b
David Gohel <david.gohel@ardata.fr>
2025-05-18 15:31:15 +0200

    tests: add few tests for knitr-utils

--- tests/testthat/test-knitr-utils.R
+++ tests/testthat/test-knitr-utils.R
@@ -1,3 +1,120 @@
+test_that("get_default_pandoc_data_file works as expected", {
+  skip_if_not_installed("knitr")
+  skip_if_not_installed("rmarkdown")
+  skip_if_not(rmarkdown::pandoc_available())
+
+  pptx_pandoc_template <- get_default_pandoc_data_file(
+    format = "pptx"
+  )
+  expect_true(file.exists(pptx_pandoc_template))
+  layout_summary_data <- layout_summary(read_pptx(pptx_pandoc_template))
+  expect_gt(nrow(layout_summary_data), 0L)
+  expect_contains(layout_summary_data$layout, "Title and Content")
+
+  docx_pandoc_template <- get_default_pandoc_data_file(
+    format = "docx"
+  )
+  expect_true(file.exists(docx_pandoc_template))
+  doc <- read_docx(docx_pandoc_template)
+  styles <- styles_info(doc)
+  expect_contains(styles$style_name, "Normal")
+  expect_contains(styles$style_name, "heading 1")
+})
+
+test_that("get_reference_value works as expected for docx", {
+  skip_if_not_installed("knitr")
+  skip_if_not_installed("rmarkdown")
+  skip_if_not(rmarkdown::pandoc_available())
+
+  tmp_file <- tempfile(fileext = ".Rmd")
+  template_docx <- system.file(package = "officer", "template/template.docx")
+  cat(
+    "
+```{r, include=FALSE}
+library(officer)
+knitr::knit_hooks$set(dummy = function(before, options, envir) {
+  if (!before) {
+    template_docx <- get_reference_value(format = \"docx\")
+    z <- read_docx(template_docx)
+    styles_data <- styles_info(z, type = \"paragraph\")[c(\"style_name\")]
+
+    return(paste(\"```{=openxml}\", to_wml(block_table(styles_data, header = FALSE)), \"```\", sep = \"\n\"))
+  }
+})
+```
+
+```{r, dummy=TRUE}
+```
+
+",
+    file = tmp_file
+  )
+
+  out_file <- tempfile(fileext = ".docx")
+
+  rmarkdown::render(
+    input = tmp_file,
+    output_format = rmarkdown::word_document(reference_docx = template_docx),
+    output_file = out_file,
+    quiet = TRUE
+  )
+
+  z <- read_docx(out_file)
+  content_summary <- docx_summary(z)
+
+  styles <- styles_info(read_docx(), type = "paragraph")
+
+  expect_equal(content_summary$text, styles$style_name)
+})
+
+test_that("get_reference_value works as expected for pptx", {
+  skip_if_not_installed("knitr")
+  skip_if_not_installed("rmarkdown")
+  skip_if_not(rmarkdown::pandoc_available())
+
+  pptx_pandoc_template <- get_default_pandoc_data_file(
+    format = "pptx"
+  )
+  template_pptx <- tempfile(fileext = ".pptx")
+  file.copy(pptx_pandoc_template, template_pptx)
+
+  str <- "
+
+```{r, include=FALSE}
+library(officer)
+knitr::knit_hooks$set(dummy = function(before, options, envir) {
+  if (!before) {
+    template_docx <- get_reference_value(format = \"pptx\")
+    saveRDS(template_docx, options$tmp_out_file)
+    return(\"\")
+  }
+})
+```
+
+```{r, dummy=TRUE, tmp_out_file=\"%s\"}
+```
+
+"
+
+  temprds <- tempfile(fileext = ".rds")
+
+  tmp_file <- tempfile(fileext = ".Rmd")
+  cat(sprintf(str, temprds), file = tmp_file)
+
+  out_file <- tempfile(fileext = ".pptx")
+
+  rmarkdown::render(
+    input = tmp_file,
+    output_format = rmarkdown::powerpoint_presentation(
+      reference_doc = template_pptx
+    ),
+    output_file = out_file,
+    quiet = TRUE
+  )
+
+  expect_equal(basename(readRDS(temprds)), basename(template_pptx))
+})
+
 test_that("opts_current_table works as expected", {
   skip_if_not_installed("knitr")
   skip_if_not_installed("rmarkdown")
