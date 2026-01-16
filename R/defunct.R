28d8d31a4ca898132adeee41230115529f8a9b21
David Gohel <david.gohel@ardata.fr>
2026-01-16 17:19:28 +0100

    fix for pkgdown

--- R/defunct.R
+++ R/defunct.R
@@ -4,6 +4,7 @@
 #' @rdname officer-defunct
 #' @details `docx_reference_img()` is not usefull anymore.
 #' @param ... unused arguments
+#' @keywords internal
 docx_reference_img <- function(...){
   .Defunct(msg = "The `docx_reference_img()` function is no longer useful.")
 }
